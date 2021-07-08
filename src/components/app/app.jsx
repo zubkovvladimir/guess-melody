b4e346c12ea3be9bd5b13f7e575ecac50319d4d9
zubkovvladimir <njoy70@mail.ru>
2021-07-08 19:06:24 +0500

    feat: enzyme

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+
+const welcomeButtomHandler = () => { };
 
 const App = (props) => {
   const { errorCount } = props;
 
   return <WelcomeScreen
     errorCount={errorCount}
+    onWelcomeButtonClick={welcomeButtomHandler}
   />;
 };
 
--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const WelcomeScreen = (props) => {
-  const { errorCount } = props;
+  const { errorCount, onWelcomeButtonClick } = props;
 
   return (
     <section className="welcome">
       <div className="welcome__logo">
-        <img src="img/melody-logo.png" alt="Угаdдай мелодию" width="186" height="83" />
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
       </div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button
+        className="welcome__button"
+        onClick={onWelcomeButtonClick}
+      ><span className="visually-hidden">Начать игру</span></button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
@@ -22,7 +25,8 @@ const WelcomeScreen = (props) => {
 };
 
 WelcomeScreen.propTypes = {
-  errorCount: PropTypes.number.isRequired
+  errorCount: PropTypes.number.isRequired,
+  onWelcomeButtonClick: PropTypes.func.isRequired,
 }
 
 export default WelcomeScreen;
